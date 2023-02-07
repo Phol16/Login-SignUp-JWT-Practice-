31f38a34236d22738dbf671bb2c3ecc3eace6072
Phol <Pholibertlim@gmail.com>
2023-02-07 20:22:47 +0800

    added some modules and codes in serverside

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -4,13 +4,16 @@ import errors from './middleware/errors.js';
 import requestDate from './middleware/requestDate.js';
 import users from './routes/usersRoutes/users.js';
 import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(express.json()); // will parse data from the body
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}))
 app.use(morgan('dev')); // will console log the details of the endpoint
-app.use(requestDate); // will just store the date
 app.use(cors());
+app.use(requestDate); // will just store the date
 
 app.use('/users', users); // will mount the users.js file on the path /users (mounting)
 
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,19 +1,29 @@
-import app from './app.js'
-import dotenv from 'dotenv'
+import app from './app.js';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
 
 dotenv.config();
 const port = process.env.PORT || 3000;
-const DB = process.env.DATABASE
+const DB = process.env.DATABASE;
 
 mongoose.set('strictQuery', true); //disable warning for depracations
 
-mongoose.connect(DB).then((con)=>{
-  // console.log(con.connections) //console log the details of the connection
-  console.log('MongoDb Connected')
-})
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    app.listen(port, (req, res) => {
+      console.log(`App is listening to server: ${port}`);
+    });
+    // console.log(con.connections) //console log the details of the connection
+    // console.log('MongoDb Connected')
+  })
+  .catch((error) => {
+    console.log(`Did not connect : ${error}`);
+  });
 
-app.listen(port, (req, res)=>{
-  console.log(`App is listening to port: ${port}`)
-})
+// app.listen(port, (req, res)=>{
+//   console.log(`App is listening to port: ${port}`)
+// })
