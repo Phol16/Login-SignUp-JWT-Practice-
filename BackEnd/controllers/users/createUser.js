8ff47c232b991a0eed0f38df75fb7087658eafc3
Phol <Pholibertlim@gmail.com>
2023-02-09 20:07:14 +0800

    added salt to the create user controller

--- BackEnd/controllers/users/createUser.js
+++ BackEnd/controllers/users/createUser.js
@@ -1,19 +1,31 @@
 import User from '../../models/userModel/userModel.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const createUser = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const theUser = await User.create({ username, password });
+    const salt = await bcrypt.genSalt(); //creating a random salt for encryption 
+    const passwordHash = await bcrypt.hash(password, salt); //this will encrypt the password by hashing the password with salt
 
+    const newUser = new User({
+      username, 
+      password: passwordHash, // to store in db the password hash
+    })
+
+    const savedUser = await newUser.save(); // this will save the the details of the user in the db
+
+    //201 status code that means it created something 
     res.status(201).json({
       status: 'success',
       requestedAt: req.requestDate,
-      data: theUser,
+      data: savedUser,
     });
   } catch (err) {
     res.status(500).json({
       status: 'failed',
+      requestedAt: req.requestDate,
       error: err,
     });
   }
